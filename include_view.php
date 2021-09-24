c975159f64e4b9dd0029f1f05378e3f6e833c491
trooile <augusto.felipe83@gmail.com>
2021-09-23 21:37:36 -0300

    language

--- include_view.php
+++ include_view.php
@@ -3,6 +3,7 @@ include './includes/include.php';
 session_start();
 if(!isset($_SESSION['language'])){
   $_SESSION['language'] = 'en-us.php';
+  include './languages/'.$_GET['language'];
 }else if(isset($_GET['language'])){
   include './languages/'.$_GET['language'];
   $_SESSION['language'] = $_GET['language'];
