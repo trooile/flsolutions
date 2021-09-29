d2a177a777bd64031ef3f36ec9c5f3b1ef7ae682
trooile <augusto.felipe83@gmail.com>
2021-09-29 19:22:29 -0300

    .

--- user/login.php
+++ user/login.php
@@ -19,6 +19,7 @@
   <a class="text-orange" id="signup" href="register.php"><b><?php echo $_SESSION['register'];?></b></a>
 </div>
 <div class="loginlogo"></div>
+
 <script>
 grecaptcha.enterprise.ready(function() {
     grecaptcha.enterprise.execute('6Lcwm4kcAAAAAJDchOGVncUglJZqxgYFBCsa9EAA', {action: 'login'}).then(function(token) {
