5b39423b4a70b9a84eb7ddf0ade7c2069a5ae93b
Augusto Cardone <augusto.cardone@hotmail.com>
2021-10-06 18:41:48 -0300

    Criação do menu

--- user/profile.php
+++ user/profile.php
@@ -1,4 +1,5 @@
 <?php include "include_view.php";
+include "menu.php";
 $user = $controller->toUsers->getAll("id_school_unit=13")[0]; //("id=1") ("nome='Augusto'") -> para pesquisar tudo relacionado //Pode remover o último colchete para puxar tudo
 $school = $controller->toSchoolUnit->getAll($user["id_school_unit"])[0];
 
