c0e80ce73cf43ddfd4d22d8921cd5b2bc93e772f
Felipe Augusto <32917698+trooile@users.noreply.github.com>
2021-10-13 08:35:41 -0300

    Update login.php

--- user/login.php
+++ user/login.php
@@ -6,17 +6,17 @@
     <div class="panel-body">
     <form>
         <div class="mb-3">
-          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="<?php echo $_SESSION['your-email'];?>">
+          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="<?=$_SESSION['your-email']?>">
         </div>
         <div class="mb-3">
-          <input type="password" class="form-control" id="passwd" placeholder="<?php echo $_SESSION['your-passwd'];?>">
+          <input type="password" class="form-control" id="passwd" placeholder="<?=$_SESSION['your-passwd']?>">
         </div>
-        <button type="button" class="btn btn-default btn-lg btn-orange"><?php echo $_SESSION['login'];?></button>
+        <button type="button" class="btn btn-default btn-lg btn-orange"><?=$_SESSION['login']?></button>
       </form>
     </div>
   </div>
-  <label for="signup"><?php echo $_SESSION['signup1'];?></label>
-  <a class="text-orange" id="signup" href="register.php"><b><?php echo $_SESSION['register'];?></b></a>
+  <label for="signup"><?=$_SESSION['signup1']?></label>
+  <a class="text-orange" id="signup" href="register.php"><b><?=$_SESSION['register']?></b></a>
 </div>
 <div class="loginlogo"></div>
 <script type="text/javascript">
@@ -36,7 +36,7 @@
                 }
         });
     }else{
-      alert('<?php echo $_SESSION['invalidpasswd']; ?>')
+      alert('<?=$_SESSION['invalidpasswd']?>')
     }
   });
 </script>
