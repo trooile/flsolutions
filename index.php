122a519cd9d7c8ac761dfbbb60fa4fbf7b5a5b93
Felipe Augusto <32917698+trooile@users.noreply.github.com>
2021-10-13 08:37:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 <body class="indexbody">
   <div class="container-fluid">
     <img src="./images/logo.svg" class="logo">
-    <a type="button" class="btn btn-default btn-orange" href="./user/login.php"><?php echo $_SESSION['login'];?></a>
+    <a type="button" class="btn btn-default btn-orange" href="./user/login.php"><?=$_SESSION['login']?></a>
   </div>
   <br><br><br>
   <div  class="index-text">
-    <label><b><?php echo $_SESSION['text-index']; ?></b></label>
+    <label><b><?=$_SESSION['text-index']?></b></label>
   </div>
   <br><br><br>
   <div style="margin-left:35px!important;">
-    <label for="signup"><?php echo $_SESSION['signup1'];?></label>
-    <a class="text-orange" href="./user/register.php"><b><?php echo $_SESSION['register'];?></b></a>
+    <label for="signup"><?=$_SESSION['signup1']?></label>
+    <a class="text-orange" href="./user/register.php"><b><?=$_SESSION['register']?></b></a>
   </div>
 </body>
 <!- 35.199.109.22 ->
