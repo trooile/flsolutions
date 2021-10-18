4de8227a331c633d17af8bdd8953135540e42921
Augusto Cardone <augusto.cardone@hotmail.com>
2021-10-17 22:52:32 -0300

    Modificação das telas de profile

--- user/profile_cadastro.php
+++ user/profile_cadastro.php
@@ -1,6 +1,11 @@
 <?php include "include_view.php";
 $user = $controller->toUsers->getAll("id_school_unit=13")[0]; //("id=1") ("nome='Augusto'") -> para pesquisar tudo relacionado //Pode remover o último colchete para puxar tudo
 $school = $controller->toSchoolUnit->getAll($user["id_school_unit"])[0];
+
+
+function update(){
+    $controller->toSchoolUnit->update(); //(["nome"=>"Augusto","lastname"=>"Cardone"], "id=1") -> Atualizar no id "1" para o nome Augusto e last name Cardone     ---- Inserindo na coluna "nome" o nome Augusto ---- depois dos colchetes colocar o onde ("where")coluna=/valor
+}
 ?>
 
 <body class="profile">
@@ -12,7 +17,7 @@ $school = $controller->toSchoolUnit->getAll($user["id_school_unit"])[0];
             <img src="/images/profile-img.svg">
         </div>
         <div class="dados">
-            <form id="form" method="POST" action="/idade">
+            <form id="form" method="POST">
                 <label><?php echo $_SESSION['nameProfile']; ?></label>
                 <div>
                     <input type="text" name="name" class="form-control" placeholder="<?php echo $user["name"]; ?>" />
