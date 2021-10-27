c5a27b8387267f8e8fb2fd996b94d8cfe0b089f7
Augusto Cardone <augusto.cardone@hotmail.com>
2021-10-27 13:46:42 -0300

    Icone menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,6 @@
 <input type="checkbox" id="menu">
 <script src="../includes/java/bootstrap/bootstrap.min.js"></script>
-<label for="menu" id="labelmenu" >MENu</span></label>
+<label for="menu" id="labelmenu" ><img src="/images/icon-menu.svg" id="imgmenu"></span></label>
 <?php 
   if(!isset($_SESSION['language'])){
     $_SESSION['language'] = 'en-us.php';
@@ -15,7 +15,7 @@
         <ul id="ulmenu">
             <li><a href=""><?=$_SESSION['home']?></a></li>
             <li><a href=""><?=$_SESSION['profile']?></a></li>
-            <li><a href=""><?=$_SESSION['settings']?></a></li><br>
+            <li><a href=""><?=$_SESSION['settings']?></a></li>
             <li><a href=""><?=$_SESSION['logout']?></a></li>
         </ul>
     </nav>
