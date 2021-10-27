4dc4256adb1eee0ce52110eaf5aa3ae05e63bdb3
trooile <augusto.felipe83@gmail.com>
2021-10-27 20:38:45 -0300

    .

--- user/profile_cadastro_update.php
+++ user/profile_cadastro_update.php
@@ -2,8 +2,6 @@
 include "include_view.php";
 $nome = $_REQUEST["name"];
 $email = $_REQUEST["email"];
-$controller->toUsers->update(["name"=>"$nome","email"=>"$email"],"id_users=".$_SESSION['userLogged']));
-
-
+$controller->toUsers->update(["name"=>"$nome","email"=>"$email"],"id_users=".$_SESSION['userLogged']);
 ?>
 <meta http-equiv="Refresh" content="0; /user/profile.php">" />
