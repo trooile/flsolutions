dfda8a46f71a624b814e5abb76d3f0d5fc348648
trooile <augusto.felipe83@gmail.com>
2021-11-09 22:22:44 -0300

    .

--- includes/footer.php
+++ includes/footer.php
@@ -4,5 +4,4 @@
     <a class="text-reset fw-bold" href="http://35.247.251.47/" target="_blank"><img src="../images/flsolutionshx.svg" style=" max-width:100px;"></a>
   </div>
 </footer>
-</body>
 </html>
