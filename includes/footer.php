6f28d589b1a77bbf2562d31c1ed07b88a029594b
Felipe Augusto <32917698+trooile@users.noreply.github.com>
2021-11-26 16:32:28 -0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="text-center">
   <div class="text-center footer">
     © <?=date('Y')?> Copyright:
-    <a class="text-reset fw-bold" href="http://35.247.251.47/" target="_blank"><img src="../images/flsolutionshx.svg" style=" max-width:100px;"></a>
+    <a href="http://35.247.251.47" target="_blank"><img src="../images/flsolutionshx.svg" style="max-width:100px;"></a>
   </div>
 </footer>
 </html>
